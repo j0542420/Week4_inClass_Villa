fa1c0f4f0bba585db360131ba6e0c7a22dbc53e9
Jose Villa <j0542420@amarillocollege.com>
2025-04-14 15:40:11 -0500

    Commit

---
+++ project01-04.js
@@ -0,0 +1,21 @@
+/*  JavaScript 7th Edition
+    Chapter 1
+    Hands-On Project 1-4
+
+      Author: Jose Villa
+      Date:   4/14/2025
+
+    Filename: project01-04.js
+*/
+
+//define variables for home and work addresses
+
+// step 2
+let homeStreet = "1 Main St.";
+let homeCity = "Sicilia";
+let homeState = "MA";
+let homeCode = "02103";
+let workStreet = "10 Oak Ln.";
+let workCity = "Central City";
+let workState = "MA";
+let workCode = "02104";
